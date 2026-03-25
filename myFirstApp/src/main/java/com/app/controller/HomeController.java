1a10e581abca3718181e39e47925666c1000a262
raj <raj@gmail.com>
2026-03-25 10:07:31 +0530

    first commit

---
+++ myFirstApp/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	@ResponseBody
+	public String getHome() {
+		return "this is your first web page....";
+	}
+	
+	
+}
